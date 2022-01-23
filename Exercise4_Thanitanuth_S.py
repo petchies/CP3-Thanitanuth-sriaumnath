ff1609040596c3d1d4d1e254b05810c0b75e76b1
petchies <newpenta88@gmail.com>
2022-01-23 16:33:22 +0700

    Send Exercise4

---
+++ Exercise4_Thanitanuth_S.py
@@ -0,0 +1,10 @@
+Foundation_English = 70.0
+General_Business = 80.5
+Introduction_to_Computer_System = 95.0
+Computer_Programing = 99.0
+
+print("---Your---")
+print("Foundation English :" , Foundation_English)
+print("General Business :" ,General_Business)
+print("Introduction To Computer System :" ,Introduction_to_Computer_System)
+print("Computer Programing :" ,Computer_Programing)
