d3309bb86156b6b89d7722ba03094af44d76a8d1
petchies <newpenta88@gmail.com>
2022-01-30 16:08:49 +0700

    Send Exercise50

---
+++ Exercise50_Thanitanuth_S.py
@@ -0,0 +1,15 @@
+def addNumber(x, y):
+    print(x+y)
+def subtractNumber(x, y):
+    print(x-y)
+def multipleNumber(x, y):
+    print(x*y)
+def divideNumber(x, y):
+    print(x/y)
+
+x = int(input("Please Enter number of x :"))
+y = input(input("Please Enter number of y :"))
+addnumber(x, y)
+subtractNumber(x, y)
+multipleNumber(x, y)
+divideNumber(x, y)
